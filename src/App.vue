18e84d8840bdc88bcc72ceb7fd1cf1af13d5e427
worawatM <worawat.pluk@gmail.com>
2023-02-23 00:23:32 +0700

    Add V.1 (V-for)

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,31 @@
 <template>
   <div id="app">
-    <div class="todo" >
+    <div class="todo">
       <div class="task-input">
-       <input type="text" placeholder="New a Task" >
-       <button @click="New_Task('New')">Submit</button>
+        <input v-model="addtask" type="text" placeholder="New a Task" />
+        <button @click="New_Task(' ')">Submit</button>
       </div>
+
       <div class="taskbox">
-        <li class="task">
-          <label for="1">
-            <input type="checkbox" id ="1">
-            <p >Gomo not Support : Change Promotion</p>
+        <!-- <li class="task">
+          <label>
+            <input type="checkbox" id="1" />
+            <p>Gomo not Support : Change Promotion</p>
             <button @click="Edit_task()">Edit</button>
             <button @click="Delete_task()">Delete</button>
           </label>
-        </li>
-        <li class="task">
-          <label for="1">
-            <input type="checkbox" id ="1">
-            <p>Gomo not Support : Reconnect - Customer </p>
-          </label>
-        </li>
-        <li class="task">
-          <label for="1">
-            <input type="checkbox" id ="1">
-            <p>Gomo not Support : RequestSuspend - Customer </p>
-          </label>
-        </li>
-        <li class="task">
-          <label for="1">
-            <input type="checkbox" id ="1">
-            <p>Gomo not Support : RequestSuspend - Fraud</p>
-          </label>
-        </li>
-        <li class="task">
-          <label for="1">
-            <input type="checkbox" id ="1">
-            <p>Gomo not Support : Reconnect - Fraud</p>
-          </label>
-        </li>
+        </li> -->
+        
+        <div v-for="(item, index) in arr" :key="index">
+          <li class="task">
+            <label>
+              <input type="checkbox" id="1" />
+              <p>{{ item }}</p>
+              <button @click="Edit_task()">Edit</button>
+              <button @click="Delete_task()">Delete</button>
+            </label>
+          </li>
+        </div>
       </div>
     </div>
   </div>
@@ -47,28 +35,34 @@
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-
+  name: "App",
+  data() {
+    return {
+      addtask: "",
+      arr: [],
+    };
   },
-  methods:{
-    New_Task(Add){
-      if(Add === 'New')
-      console.log(Add)
-    }
-  }
-}
+  components: {},
+  methods: {
+    New_Task() {
+      // this.i++
+      this.arr.push(this.addtask)
+      this.addtask = ""
+      console.log(this.arr)
+    },
+  },
+};
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
-ิbody{
+ิbody {
   /* display: none; */
-  background: #3c87ff ;
+  background: #3c87ff;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -76,11 +70,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   /* color: #2c3e50; */
 }
-.taskbox .task{
+.taskbox .task {
   list-style: none;
 }
-.task label{
+.task label {
   display: flex;
 }
-
 </style>
